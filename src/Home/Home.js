13cf65102adef20938cb0229171530e45c1642f3
unknown <sazid7046@gmail.com>
2021-08-27 01:19:53 +0530

    add footer

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Hero from './Hero';
+import Footer from '../components/ui/Footer';
 
 function Home() {
   return (
     <React.Fragment>
       <Hero/>
+      <Footer/>
     </React.Fragment>
   )
 }
