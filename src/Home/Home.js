5968853d2098a29e7817164c0c84baae7714815b
unknown <sazid7046@gmail.com>
2021-08-27 01:18:25 +0530

    footer delete

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,14 +1,11 @@
 import React from 'react'
 import Hero from './Hero';
-import Footer from '../components/ui/Footer';
 
 function Home() {
   return (
     <React.Fragment>
       <Hero/>
-      <Footer/>
     </React.Fragment>
   )
 }
-
 export default Home
